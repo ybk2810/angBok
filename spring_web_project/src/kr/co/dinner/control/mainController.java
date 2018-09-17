433329009a87847172faf67c699ec6e909f90214
suho <suho@gmail.com>
2018-09-17 10:24:48 +0900

    09/17categoryupdate

--- spring_web_project/src/kr/co/dinner/control/mainController.java
+++ spring_web_project/src/kr/co/dinner/control/mainController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.dinner.dto.MagazineDTO;
@@ -55,4 +56,13 @@ public class mainController {
 	
 /*	@RequestMapping("/mypage.do")
 	public ModelAndView */
+	@RequestMapping("/category.do")
+	public ModelAndView categoryList(@RequestParam("category")String category) {
+		ModelAndView mav = new ModelAndView();
+		List<WritingDTO> clist = ws.readAll(category);
+		System.out.println(clist);
+		mav.addObject("clist", clist);
+		mav.setViewName("plates");
+		return mav;
+	}
 }
