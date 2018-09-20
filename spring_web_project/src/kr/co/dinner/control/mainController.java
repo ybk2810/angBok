c0869e336951d6148043d18f933829f92353a7c8
suho <suho@gmail.com>
2018-09-20 17:11:00 +0900

    2121

--- spring_web_project/src/kr/co/dinner/control/mainController.java
+++ spring_web_project/src/kr/co/dinner/control/mainController.java
@@ -212,6 +212,16 @@ public class mainController {
 		return "main";
 	}
 	
+	@RequestMapping("/writerPage.do")
+	public ModelAndView writerPage(@RequestParam("id")String id) {
+		ModelAndView mav = new ModelAndView();
+		MemberDTO dto = member.selectIdOne(id);
+		
+		mav.addObject("mdto", dto);
+		mav.setViewName("writerPage");
+		
+		return mav;
+	}
 	
 }
 
